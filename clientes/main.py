e197ecdd31306ee24e9be2370190ee51de9647a0
Liceth Yaneth <liceth.rozo@finkargo.com>
2024-10-13 21:45:59 -0500

    BA-12 Add unitest create client

--- clientes/main.py
+++ clientes/main.py
@@ -1,15 +1,16 @@
-# app/main.py
 from fastapi import FastAPI
 from app.routers import cliente
 from app.db.session import engine
 from app.db.base import Base
 
 def recreate_database():
+    print("Recreating the database...")
     Base.metadata.create_all(engine)
-
-recreate_database()
 
 app = FastAPI()
 
 app.include_router(cliente.router)
-print("main.py")
+
+
+if __name__ == "__main__":
+    recreate_database()
